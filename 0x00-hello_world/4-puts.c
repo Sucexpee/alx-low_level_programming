b1d2bf6728ba25107974523cfa8620af3a6846b7
Sucexpee <successfulfeji@gmail.com>
2023-06-15 13:52:23 -0700

    betty clone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line with put function
-Return : (0) (success)
+ * main - A program that prints a line with put function
+*Return: (0) (success)
 */
 int main(void)
 {
