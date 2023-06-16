a03a1b02f8af2a285e159dbc44b1f12984eabe6a
Sucexpee <successfulfeji@gmail.com>
2023-06-16 11:53:49 -0700

    betty clone

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*Return: (0) (success)
-*/
+ * main - Entry point
+ *Program written by idogun success
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
