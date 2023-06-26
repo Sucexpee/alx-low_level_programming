08cf25a00e56fda51a8039f712f4ad978bd60e34
Sucexpee <successfulfeji@gmail.com>
2023-06-26 08:35:09 -0700

    strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+}
