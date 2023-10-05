2ae11f08965b258f0ae4a376c39a28352a01ee74
sucexpee <successfulfeji@gmail.com>
2023-10-05 10:08:50 -0700

    Fixed git push issue

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * create_file - This helpsto  Create a file.
+ * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
- * @text_content: pointer to a string to write to the file.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: If function fails - -1.
+ * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
